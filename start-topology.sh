2d13821a83ca73b636129b84753c13f6c60790a4
Everson <eversonborges@gmail.com>
2022-04-28 14:44:00 -0300

    .

--- start-topology.sh
+++ start-topology.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-# Copyright [2019-2022] Universidade Federal do Espirito Santo
-#                       Instituto Federal do Espirito Santo
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#Here you must add the path according your system operation 
-RTR=/<path freertr>/rtr.jar
-#Here you must add the path according your system operation 
-HWSW=/<path hardware and software files>/
-
+RTR=/Users/everson/Dropbox/rtr/rtr.jar
+HWSW=/Users/everson/Dropbox/rtr/everson/wpeif2022/polka-ospf/
 
 tmux new-session -d -s rare 'java -jar '$RTR' routersc '$HWSW'r1-hw.txt '$HWSW'r1-sw.txt'
 tmux split-window -v -t 0 -p 50
@@ -29,8 +11,10 @@ tmux send 'java -jar '$RTR' routersc '$HWSW'r3-hw.txt '$HWSW'r3-sw.txt' ENTER;
 tmux split-window -h -t 2 -p 50
 tmux send 'java -jar '$RTR' routersc '$HWSW'r4-hw.txt '$HWSW'r4-sw.txt' ENTER;
 tmux split-window -v -t 0 -p 50
-tmux send 'java -jar '$RTR' routersc '$HWSW'r5-hw.txt '$HWSW'r5-sw.txt' ENTER;
+tmux send 'java -jar '$RTR' routersc '$HWSW'r7-hw.txt '$HWSW'r7-sw.txt' ENTER;
 tmux split-window -v -t 2 -p 50
 tmux send 'java -jar '$RTR' routersc '$HWSW'r6-hw.txt '$HWSW'r6-sw.txt' ENTER;
+tmux split-window -v -t 2 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r5-hw.txt '$HWSW'r5-sw.txt' ENTER;
 tmux select-layout tiled
 tmux a;
